a5be901b150d0c5595b15409866e417505628690
mariashka <bisyarinamariashka@gmail.com>
2015-09-21 15:06:58 +0300

    Add something for visualization of trapezoids

--- visualize.cpp
+++ visualize.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+const int WIDTH = 1300;
+const int HEIGHT = 750;
+
+
 struct point {
   int x, y;
 
@@ -19,6 +23,10 @@ struct point {
   point(int a, int b) {
     x = a;
     y = b;
+  }
+
+  bool operator < (point a) {
+    return (x < a.x) || ((x == a.x) && (y < a.y));
   }
 };
 
@@ -33,12 +41,70 @@ struct segment {
   segment(int a, int b, int c, int d) {
     start = point(a, b);
     end = point(c, d);
+    if (end < start) {
+      point x = start;
+      start = end;
+      end = x;
+    }
   }
-                         
+
+  void draw() {
+    fl_color(FL_BLACK);
+    fl_line(start.x, HEIGHT - start.y, end.x, HEIGHT - end.y);
+  }
 };
 
-const int WIDTH = 1300;
-const int HEIGHT = 750;
+int projection(point a, segment b) {
+  if (a.x == b.start.x) {
+    return b.start.y;
+  } else if (a.x == b.end.x) {
+    return b.end.y;
+  } else {
+    return b.start.y + ((b.end.y - b.start.y) * (a.x - b.start.x)) / (b.end.x - b.start.x);
+  }
+}
+
+struct trapezoid {
+  segment top, bottom;
+  point left, right;
+  trapezoid *up, *down, *next, *prev;
+
+  trapezoid(segment a, segment b, point c, point d) {
+    top = a;
+    bottom = b;
+    left = c;
+    right = d;
+  }
+
+  void draw() {
+    top.draw();
+    bottom.draw();
+
+    int left_high_end = HEIGHT;
+    int left_low_end = 1;
+    int right_high_end = HEIGHT;
+    int right_low_end = 1;
+    if (left.x >= top.start.x && left.x <= top.end.x) {
+      left_high_end = projection(left, top);
+    }
+    if (left.x >= bottom.start.x && left.x <= bottom.end.x) {
+      left_low_end = projection(left, bottom);
+    }
+    if (right.x >= top.start.x && right.x <= top.end.x) {
+      right_high_end = projection(right, top);
+    }
+    if (right.x >= bottom.start.x && right.x <= bottom.end.x) {
+      right_low_end = projection(right, bottom);
+    }
+    
+    fl_color(FL_RED);
+    segment(left.x, left_high_end, left.x, left_low_end).draw();
+    segment(right.x, right_high_end, right.x, right_low_end).draw();
+  }
+
+  ~trapezoid() {}
+};
+
 
 struct TMap_Widget:Fl_Widget {
   vector <segment> segments;
@@ -46,10 +112,16 @@ struct TMap_Widget:Fl_Widget {
 
   void draw() {
     fl_draw_box	(FL_FLAT_BOX, 0, 0, WIDTH, HEIGHT, FL_WHITE);		
-    fl_color(FL_BLACK);
     for (int i = 0; i < (int)segments.size(); ++i) {
-      fl_line(segments[i].start.x, segments[i].start.y, segments[i].end.x, segments[i].end.y);
+      segments[i].draw();
     }
+
+    segment b(200, 200, 300, 300);
+    segment a(100, 100, 400, 100);
+    point c(250, 250);
+    point d(300, 300);
+    trapezoid t(b, a, c, d);
+    t.draw();
   }
 };
 
