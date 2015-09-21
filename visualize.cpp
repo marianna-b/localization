9268246ce46ed4fa566db7a64fe1cf85798f647e
mariashka <bisyarinamariashka@gmail.com>
2015-09-21 13:08:21 +0300

    Add visualization of set of segments using FLTK

--- visualize.cpp
+++ visualize.cpp
@@ -4,11 +4,10 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/fl_draw.H>
+#include <FL/x.H> 
 
 using namespace std;
-
-const int SCREEN_WIDTH = 700;
-const int SCREEN_HEIGHT = 700;
 
 struct point {
   int x, y;
@@ -38,23 +37,35 @@ struct segment {
                          
 };
 
+const int WIDTH = 1300;
+const int HEIGHT = 750;
+
+struct TMap_Widget:Fl_Widget {
+  vector <segment> segments;
+  TMap_Widget(int x, int y, int w, int h, const char *label = 0) : Fl_Widget(x, y, w, h, label) {}
+
+  void draw() {
+    fl_draw_box	(FL_FLAT_BOX, 0, 0, WIDTH, HEIGHT, FL_WHITE);		
+    fl_color(FL_BLACK);
+    for (int i = 0; i < (int)segments.size(); ++i) {
+      fl_line(segments[i].start.x, segments[i].start.y, segments[i].end.x, segments[i].end.y);
+    }
+  }
+};
+
 int main(int argc, char** argv) {
-  /*
   int n, a, b, c, d;
   vector<segment> v;
+  
   cin >> n;
-  for (auto i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
     cin >> a >> b >> c >> d; 
     v.push_back(segment(a, b, c, d));
   }
-  */
 
-  Fl_Window *window = new Fl_Window(340,180);
-  Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
-  box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD + FL_ITALIC);
-  box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
+  Fl_Window *window = new Fl_Window(WIDTH, HEIGHT);
+  TMap_Widget *tmap = new TMap_Widget(0, 0, WIDTH, HEIGHT);
+  tmap->segments = v;
   window->end();
   window->show(argc, argv);
   return Fl::run();
